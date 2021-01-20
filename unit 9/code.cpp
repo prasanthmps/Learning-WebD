872362432f3ed85725f47cb40d6446e344d65bee
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-01-20 22:46:10 +0530

    CHANGES

---
+++ unit 9/code.cpp
@@ -0,0 +1,34 @@
+
+struct node *delete(struct node *root, int key) {
+  // Return if the tree is empty
+  if (root == NULL) return root;
+
+  // Find the node to be deleted
+  if (key < root->key)
+    root->left = Node(root->left, key);
+  else if (key > root->key)
+    root->right = Node(root->right, key);
+  else {
+
+    // If the node is with only one child or no child
+    if (root->left == NULL) {
+      struct node *temp = root->right;
+      free(root);
+      return temp;
+    } else if (root->right == NULL) {
+      struct node *temp = root->left;
+      free(root);
+      return temp;
+    }
+
+    // If the node has two children
+    struct node *temp = minValueNode(root->right);
+
+    // Place the inorder successor in position of the node to be deleted
+    root->key = temp->key;
+
+    // Delete the inorder successor
+    root->right = deleteNode(root->right, temp->key);
+  }
+  return root;
+}
