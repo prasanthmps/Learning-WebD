74205300fea69bd713548045962c2c0c7ce5c02e
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-03-29 16:17:03 +0530

    comm

--- NodeJs/jokes/index.js
+++ NodeJs/jokes/index.js
@@ -2,7 +2,18 @@ const jokes1 = require("give-me-a-joke");
 var colors = require('colors');
 var giveMeAJoke = require('give-me-a-joke');
  
-// To get a random dad joke
-giveMeAJoke.getRandomDadJoke (function(joke) {
-     console.log(joke.rainbow);
+var figlet = require('figlet');
+ 
+figlet('Hello World!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
+    }
+    console.log(data.rainbow);
 });
+
+// To get a random dad joke
+// giveMeAJoke.getRandomDadJoke (function(joke) {
+//      console.log(joke.rainbow);
+// });
