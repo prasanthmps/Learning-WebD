d03791637190ae53c1e695f0f52d758fad9a251a
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-04-05 11:01:10 +0530

    commit 3

--- Express/Templating/index.js
+++ Express/Templating/index.js
@@ -1,8 +1,10 @@
 const express=require('express');
 const app=express();
+const path=require('path');
 app.set('view engine','ejs');
+app.set('views',path.join(__dirname,'/views'))
 app.get('/',(req,res)=>{
-    // res.render('home');
+    res.render('home');
 })
 app.listen(8080,()=>{
     console.log('Listenng on 3000');
