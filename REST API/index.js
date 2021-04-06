4ace86a773e1891fbea6bb36b823d13a00aaad56
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-04-06 12:31:32 +0530

    commit 2

--- REST API/index.js
+++ REST API/index.js
@@ -1,10 +1,34 @@
 const express =require('express');
 const app=express();
-
+const path=require('path');
 
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs')
+
+const comments =[
+    {
+        username:'pom',
+        comment:'heloooo'
+    },
+    {
+        username:'com',
+        comment:'heyyyy'
+    },
+    {
+        username:'dom',
+        comment:'all the best'
+    },
+    {
+        username:'tom',
+        comment:'just seeing'
+    }
+]
+app.get('/comments',(req,res)=>{
+    res.render('comments/index',{comments})
+})
 app.get('/tacos',(req,res)=>{
     res.send("GET /tacos response")
 })
