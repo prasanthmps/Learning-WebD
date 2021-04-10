f3c27df1af1a8d013a7e4ff9198771d073993d76
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-04-10 23:39:29 +0530

    commit 10/4

--- REST API/index.js
+++ REST API/index.js
@@ -1,7 +1,7 @@
 const express =require('express');
 const app=express();
 const path=require('path');
-
+const { v4: uuid } = require('uuid');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
@@ -10,18 +10,22 @@ app.set('view engine','ejs')
 
 const comments =[
     {
+        id:uuid(),
         username:'pom',
         comment:'heloooo'
     },
     {
+        id:uuid(),
         username:'com',
         comment:'heyyyy'
     },
     {
+        id:uuid(),
         username:'dom',
         comment:'all the best'
     },
     {
+        id:uuid(),
         username:'tom',
         comment:'just seeing'
     }
@@ -30,12 +34,22 @@ app.get('/comments',(req,res)=>{
     res.render('comments/index',{comments})
 })
 app.post('/comments',(req,res)=>{
-    console.log(req.body)
-    res.send("IT Worked");
+    const{username,comment}=req.body;
+    comments.push({username,comment});
+    res.redirect('/comments');
 })
+ 
 app.get('/comments/new',(req,res)=>{
     res.render('comments/new')
 })
+
+app.get('/comments/:id',(req,res)=>{
+    const{id}=req.params;
+    const comment =comments.find(c=>c.id=== id)
+    // res.render('/comments/poi')
+   res.render('comments/show',{comment})
+})
+
 app.listen(3000,()=>{
     console.log("ON PORT  3000")
 })
