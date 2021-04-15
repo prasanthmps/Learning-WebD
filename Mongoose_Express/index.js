51e53637346ccebe202fa5732a2347b490a2ccbf
Madaka Purna Sai Prasanth <prasanthmps789@gmail.com>
2021-04-15 12:29:48 +0530

    commit

---
+++ Mongoose_Express/index.js
@@ -0,0 +1,48 @@
+const express= require('express');
+const app=express();
+const path=require('path');
+const mongoose = require('mongoose');
+const Product =require('./models/product');
+mongoose.connect('mongodb://localhost:27017/farmStand', { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => {
+        console.log(" mongo CONNECTION OPEN!!!")
+    })
+    .catch(err => {
+        console.log("OH NO ERROR!!!!")
+        console.log(err)
+    })
+
+
+
+app.set('views',path.join(__dirname,'views'));
+app.set('view engine','ejs');
+app.use(express.urlencoded({extended:true}))
+const categories = ['fruit', 'vegetable', 'dairy'];
+
+
+app.get('/products', async (req,res)=>{
+    const { category } = req.query;
+    if (category) {
+        const products = await Product.find({ category })
+        res.render('products/index', { products, category })
+    } else {
+        const products = await Product.find({})
+        res.render('products/index', { products, category: 'All' })
+    }
+})
+app.get('/products/new',(req,res)=>{
+    res.render('products/new',{ categories })
+})
+app.post('/products',async (req,res)=>{
+        const newProduct = new Product(req.body)
+        await newProduct.save();
+        res.redirect(`/products/${newProduct._id}`)
+})
+app.get('/products/:id',async (req,res)=>{
+    const {id}=req.params;
+    const product =await Product.findById(id);
+    res.render('products/show',{product})
+})
+app.listen(3000,()=>{
+    console.log("App is listening");
+})
